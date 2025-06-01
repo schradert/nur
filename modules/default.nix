3c8228484597a8a75e774ccbc1cf629633dd083c
Tristan Schrader <t0rdos@pm.me>
2025-06-01 12:56:49 -0700

    feat: ladybird package override with desktop item

--- modules/default.nix
+++ modules/default.nix
@@ -3,6 +3,7 @@
     ./angle.nix
     ./cli-pride-flags.nix
     ./gtt.nix
+    ./ladybird.nix
     ./lockscreen-widgets
     ./zellij-plugins
   ];
---
+++ modules/ladybird.nix
@@ -0,0 +1,21 @@
+{
+  flake.overlays.ladybird = final: prev: {
+    ladybird = prev.ladybird.overrideAttrs (old: {
+      nativeBuildInputs = old.nativeBuildInputs ++ [final.copyDesktopItems];
+      buildInputs = old.buildInputs ++ [final.lcms] ++ final.lib.optional final.stdenv.hostPlatform.isDarwin final.unofficial-angle;
+      desktopItems = [
+        (final.makeDesktopItem {
+          name = "ladybird";
+          desktopName = "Ladybird";
+          exec = "Ladybird";
+          icon = "ladybird";
+          comment = old.meta.description;
+          categories = ["Network" "InstantMessaging"];
+          startupWMClass = "Ladybird";
+          terminal = false;
+        })
+      ];
+      postInstall = old.postInstall + "install -D $out/share/Lagom/icons/128x128/app-browser-dark.png $out/share/icons/hicolor/128x128/apps/ladybird.png";
+    });
+  };
+}
